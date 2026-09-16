810edb6c7a8f4f77e9869540a8b0611815afc909
Sergey Parshin <info@sergeyparshin.ru>
2026-09-16 16:50:04 +0000

    Variant M: exploding device reveals contacts, minimal (name + contacts only)

--- vars.js
+++ vars.js
@@ -1,7 +1,7 @@
 /* shared variant switcher A-J */
 (function(){
   const v=document.getElementById('vars'); if(!v) return;
-  const pages=[['A','index.html'],['B','b.html'],['C','c.html'],['D','d.html'],['E','e.html'],['F','f.html'],['G','g.html'],['H','h.html'],['I','i.html'],['J','j.html'],['K','k.html'],['L','l.html']];
+  const pages=[['A','index.html'],['B','b.html'],['C','c.html'],['D','d.html'],['E','e.html'],['F','f.html'],['G','g.html'],['H','h.html'],['I','i.html'],['J','j.html'],['K','k.html'],['L','l.html'],['M','m.html']];
   const cur=(location.pathname.split('/').pop()||'index.html');
   v.innerHTML=pages.map(([l,f])=>`<a href="${f}" class="${f===cur?'on':''}">${l}</a>`).join('');
 })();
