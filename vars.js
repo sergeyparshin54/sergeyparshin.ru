9260443b54e8a7feb2123993b9856fdef5fda798
sergeyparshin54 <sergeyparshin54@users.noreply.github.com>
2026-09-16 20:13:08 +0000

    Single-page site: variant O becomes index.html, all other pages removed

--- vars.js
+++
@@ -1,7 +0,0 @@
-/* shared variant switcher A-J */
-(function(){
-  const v=document.getElementById('vars'); if(!v) return;
-  const pages=[['A','index.html'],['B','b.html'],['C','c.html'],['D','d.html'],['E','e.html'],['F','f.html'],['G','g.html'],['H','h.html'],['I','i.html'],['J','j.html'],['K','k.html'],['L','l.html'],['M','m.html'],['N','n.html'],['O','o.html']];
-  const cur=(location.pathname.split('/').pop()||'index.html');
-  v.innerHTML=pages.map(([l,f])=>`<a href="${f}" class="${f===cur?'on':''}">${l}</a>`).join('');
-})();
